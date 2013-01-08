12a32ef503b0a6171d671a87dc68b2f43903614b
twinturbotom <tommmello@gmail.com>
2013-01-08 08:58:07 -0500

    customer uploads working

--- app/models/cust_upload.rb
+++ app/models/cust_upload.rb
@@ -1,3 +1,8 @@
 class CustUpload < ActiveRecord::Base
   attr_accessible :cust_file_url, :name
+  before_create :default_name
+
+  def default_name
+  	self.name ||= File.basename(cust_file_url, '.*').titleize if cust_file_url
+  end
 end
